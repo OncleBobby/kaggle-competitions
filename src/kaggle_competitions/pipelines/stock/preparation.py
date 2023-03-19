d12083f9c76595e30f9afc0ac8fcdeb720a9b1bd
OncleBobby <gpontus0807@gmail.com>
2023-03-19 07:16:38 +0100

    improve preparation

--- src/kaggle_competitions/pipelines/stock/preparation.py
+++ src/kaggle_competitions/pipelines/stock/preparation.py
@@ -11,11 +11,11 @@ def choose_files(parameters, x_train_raw, y_train_raw, x_test_raw, y_test_raw):
     return format_x(parameters, x_train_raw), y_train_raw, format_x(parameters, x_test_raw), y_test_raw
 def format_x(parameters, x):
     # ID	day	equity	r0	r1	r2	r3	r4	r5	r6	...	r44	r45	r46	r47	r48	r49	r50	r51	r52
-    columns = ['ID', 'day', 'equity']
+    columns = ['ID', 'day', 'equity', 'nbr_positif', 'nbr_negatif']
     columns.extend([f'r{i}' for i in range(40, 53)])
     # columns.extend([f'r{i}' for i in range(0, 53)])
     x_formatted = x.fillna(0)
-    return x_formatted[columns]
+    return _add_columns(x_formatted)[columns]
 def split(x, y, test_size):
     days = x['day'].unique()
     days.sort()
@@ -30,5 +30,22 @@ def split(x, y, test_size):
     y_test = y[y['ID'].isin(x_test['ID'].unique())]
 
     logging.info(f'x={len(x.index)},y={len(y.index)},x_train={len(x_train.index)},x_test={len(x_test.index)},y_train={len(y_train.index)},y_test={len(y_test.index)}')
-
     return x_train, x_test, y_train, y_test
+def _add_columns(x):
+    logging.info('Adding columns ...')
+    def update_columns(row):
+        nbr_positif = 0
+        nbr_negatif = 0
+        for j in range(0, 53):
+            if row[j] > 0:
+                nbr_positif = nbr_positif + 1
+            if row[j] < 0:
+                nbr_negatif = nbr_negatif + 1
+        row['nbr_positif'] = nbr_positif
+        row['nbr_negatif'] = nbr_negatif
+        return row
+    x['nbr_positif'] = 0
+    x['nbr_negatif'] = 0
+    x = x.apply(update_columns, axis=1)
+    logging.info('Adding columns done !')
+    return x
